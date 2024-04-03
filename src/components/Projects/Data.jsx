de576e60975681f3a9552541fd9d9f4910788442
Arunteja27 <aruntejavk@gmail.com>
2024-04-03 15:28:24 -0400

    updated skills section, typos, added in project images with their github link

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -1,8 +1,9 @@
-import Work1 from "../../assets/work1.jpg";
-import Work2 from "../../assets/work2.jpg";
-import Work3 from "../../assets/work3.jpg";
-import Work4 from "../../assets/work4.jpg";
-import Work5 from "../../assets/work5.jpg";
+import Work1 from "../../assets/portfolio-website.png";
+import Work2 from "../../assets/nerd-express.jpeg";
+import Work3 from "../../assets/save-soil.png";
+import Work4 from "../../assets/vibe.jpeg";
+import Work5 from "../../assets/catch-that-fridge.png";
+import Work6 from "../../assets/stonks.jpeg";
 
 export const projectsData = [
   {
@@ -10,30 +11,42 @@ export const projectsData = [
     image: Work1,
     title: "Portfolio Website",
     category: "web",
+    link: "https://github.com/Arunteja27/arun-Website"
   },
   {
     id: 2,
     image: Work2,
     title: "Nerd Express",
     category: "backend",
+    link: "https://github.com/Arunteja27/Nerd-Express"
   },
   {
     id: 3,
     image: Work3,
     title: "Email bot for Save Soil",
     category: "automation",
+    link: "https://github.com/Arunteja27/Save-Soil-Web-Scraper"
   },
   {
     id: 4,
     image: Work4,
     title: "Vibe-ify",
     category: "backend",
+    link: "https://github.com/Arunteja27/Vibe-ify"
   },
   {
     id: 5,
     image: Work5,
-    title: "Community Fridge",
+    title: "Catch that fridge!",
     category: "web",
+    link: "https://github.com/Arunteja27/Catch-your-fridge-Express-server"
+  },
+  {
+    id: 6,
+    image: Work6,
+    title: "JP Morgan Stock Ticker Tracker",
+    category: "web",
+    link: "https://github.com/Arunteja27/jpmc-task-1"
   },
 ];
 
---
+++ src/components/skills/Tools.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+const Tools = () => {
+  return (
+    <div className="skills__content">
+      <h3 className="skills__title">Developer Tooling</h3>
+
+      <div className="skills__box">
+        <div className="skills__group">
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Git</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">MongoDB</h3>
+              <span className="skills__level">Intermediate</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Docker</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+        </div>
+
+        <div className="skills__group">
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">AWS</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Spring Boot</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+          <div className="skills__data">
+            <i class="bx bx-badge-check"></i>
+
+            <div>
+              <h3 className="skills__name">Redis</h3>
+              <span className="skills__level">Basic</span>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Tools;
