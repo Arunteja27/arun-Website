74c36b41b6a0fe7ebc57d29ee1716d138b90f015
Arunteja27 <aruntejavk@gmail.com>
2024-12-12 23:21:48 -0500

    new project pog, brightened pfp and minor other edits kekek

--- src/components/Projects/Data.jsx
+++ src/components/Projects/Data.jsx
@@ -5,6 +5,7 @@ import Work4 from "../../assets/vibe.jpeg";
 import Work5 from "../../assets/catch-that-fridge.png";
 import Work6 from "../../assets/stonks.jpeg";
 import Work7 from "../../assets/SmartWardrobe.png";
+import Work8 from "../../assets/radotech.webp"
 
 export const projectsData = [
   // {
@@ -16,46 +17,55 @@ export const projectsData = [
   // },
   {
     id: 2,
+    image: Work8,
+    title: "RaDoTech Health Monitoring Simulation",
+    category: "backend",
+    link: "https://github.com/Cotni/3004-project"
+  },
+  {
+    id: 3,
     image: Work5,
     title: "Catch that fridge!",
     category: "web",
     link: "https://github.com/Arunteja27/Catch-your-fridge-Express-server"
   },
   {
-    id: 3,
+    id: 4,
     image: Work4,
     title: "Vibe-ify",
     category: "backend",
     link: "https://github.com/Arunteja27/Vibe-ify"
   },
   {
-    id: 7,
-    image: Work6,
-    title: "JP Morgan Stock Ticker Tracker",
+    id: 5,
+    image: Work1,
+    title: "Portfolio Website",
     category: "web",
-    link: "https://github.com/Arunteja27/jpmc-task-1"
+    link: "https://github.com/Arunteja27/arun-Website"
   },
   {
-    id: 5,
+    id: 6,
     image: Work2,
     title: "Nerd Express",
     category: "backend",
     link: "https://github.com/Arunteja27/Nerd-Express"
   },
   {
-    id: 6,
+    id: 7,
     image: Work3,
     title: "Email bot for Save Soil",
     category: "automation",
     link: "https://github.com/Arunteja27/Save-Soil-Web-Scraper"
   },
   {
-    id: 4,
-    image: Work1,
-    title: "Portfolio Website",
+    id: 8,
+    image: Work6,
+    title: "JP Morgan Stock Ticker Tracker",
     category: "web",
-    link: "https://github.com/Arunteja27/arun-Website"
+    link: "https://github.com/Arunteja27/jpmc-task-1"
   },
+
+
 ];
 
 // projects
